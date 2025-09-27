ab2a07b5b24ca560257e26a02441d0ba9bf2672f
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-09-27 19:51:51 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Старт моего проекта
+ */
+public class Solution {
+
+    /**
+     * Точка входа в проект и печать Hello world!
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
