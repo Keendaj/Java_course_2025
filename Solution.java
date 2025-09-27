0bc12223a6ff8fc15435e4fb8cee84c3f0830c17
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-09-27 19:53:49 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,11 +1,15 @@
 /**
  * Старт моего проекта
+ * @author Dmitriy Shevtsov
+* @verision 1.0
+* @since 2025
  */
 public class Solution {
 
     /**
      * Точка входа в проект и печать Hello world!
      * @param args
+     * @return void
      */
     public static void main(String[] args){
         System.out.println("Hello world!");
