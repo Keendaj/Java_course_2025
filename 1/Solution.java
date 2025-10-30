874546164c4f9ce95b6e19c25aebc71eb4108602
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-10-30 14:22:01 +0300

    Done

--- 1/Solution.java
+++ 1/Solution.java
@@ -1,10 +1,45 @@
-class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+class MyClass {
+    private int id;
+    private String name;
+    
+    public MyClass(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public int add(int a, int b, int c) {
-        return a + b + c;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        MyClass other = (MyClass) obj;
+        
+        if (id != other.id) {
+            return false;
+        }
+
+        if (name == null) {
+            return other.name == null;
+        } else {
+            return name.equals(other.name);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
 
@@ -19,12 +54,34 @@ public class Solution {
     /**
      * @param args
      * @return void
-     * @see Показывает примеры работы с перегруженными методами. 
+     * @see Показывает пример переопределния equals. 
+     * Метод должен удовлетворять таким критериям:
+     * 1) Симметричность
+     * 2) Рефлексивность
+     * 3) Транзитивность
+     * 4) Согласованность
+     * 5) Если null, то всегда false
+     * 6) Согласованность с hashCode (если коды равны, то и equals выдаст true)
      */
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        MyClass obj1 = new MyClass(1, "Test");
+        MyClass obj2 = new MyClass(1, "Test");
+        MyClass obj3 = new MyClass(2, "Test");
+        MyClass obj4 = new MyClass(1, "Other");
         
-        System.out.println("add(5, 3): " + calc.add(5, 3));
-        System.out.println("add(1, 2, 3): " + calc.add(1, 2, 3));
+        System.out.println("obj1.equals(obj1): " + obj1.equals(obj1));
+        System.out.println("obj1.equals(obj2): " + obj1.equals(obj2));
+        System.out.println("obj2.equals(obj1): " + obj2.equals(obj1));
+        System.out.println("obj1.equals(obj3): " + obj1.equals(obj3));
+        System.out.println("obj1.equals(obj4): " + obj1.equals(obj4));
+        System.out.println("obj1.equals(null): " + obj1.equals(null));
+
+        System.out.println("\nobj1.hashCode(): " + obj1.hashCode());
+        System.out.println("obj2.hashCode(): " + obj2.hashCode());
+        System.out.println("obj3.hashCode(): " + obj3.hashCode());
+        
+        System.out.println("\nСогласованность equals и hashCode:");
+        System.out.println("obj1.equals(obj2) && obj1.hashCode() == obj2.hashCode(): " + 
+                          (obj1.equals(obj2) && obj1.hashCode() == obj2.hashCode()));
     }
 }
