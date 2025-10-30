9263972bb0053da368589a021725d4aebf5e56f7
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-10-30 13:55:51 +0300

    Done

--- 1/Solution.java
+++ 1/Solution.java
@@ -1,4 +1,27 @@
+class Animal {
+    public final void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
 
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating dog food");
+    }
+    
+    // Вызовет ошибку, если раскомментировать
+    /*
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks");
+    }
+    */
+}
 /**
  * Старт моего проекта
  * @author Dmitriy Shevtsov
@@ -6,53 +29,15 @@
  * @since 2025
  */
 public class Solution {
-    
-    public enum CoffeeSize {
-        SMALL(200, 100),
-        MEDIUM(300, 150), 
-        LARGE(500, 200);
-
-        private final int ml;
-        private final int price;
-
-        CoffeeSize(int ml, int price) {
-            this.ml = ml;
-            this.price = price;
-        }
-
-        public int getMl() {
-            return ml;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-    }
-
-    public enum Color {
-        RED, GREEN, BLUE, YELLOW, BLACK, WHITE
-    }
 
     /**
      * @param args
      * @return void
-     * @see Показывает примеры работы с enum. 
+     * @see Показывает примеры работы с final методом. 
      */
     public static void main(String[] args) {
-        System.out.println("\n=== ПРИМЕР 1: обычный enum ===");
-        Color favoriteColor = Color.BLUE;
-        System.out.println("Мой любимый цвет: " + favoriteColor);
-        
-        for (Color color : Color.values()) {
-            System.out.println("Цвет: " + color);
-        }
-
-        System.out.println("=== ПРИМЕР 2: enum с заданными значениями ===");
-        CoffeeSize myCoffee = CoffeeSize.LARGE;
-        System.out.println("Мой кофе: " + myCoffee + " - " + myCoffee.getMl() + " мл, " + myCoffee.getPrice() + " руб.");
-        
-        for (CoffeeSize size : CoffeeSize.values()) {
-            System.out.println(size + ": " + size.getMl() + " мл, " + size.getPrice() + " руб.");
-        }
+        Dog dog = new Dog();
+        dog.makeSound();
+        dog.eat();      
     }
 }
