dfa70d65246ffb97e8ef0b3ff25c5d9bf692c69b
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-10-31 15:18:31 +0300

    Done

--- 1/Solution.java
+++ 1/Solution.java
@@ -1,5 +1,5 @@
 /**
- * Старт моего проекта
+ * Демонстрация работы со StringBuilder
  * @author Dmitriy Shevtsov
  * @version 1.0
  * @since 2025
@@ -9,43 +9,50 @@ public class Solution {
     /**
      * @param args
      * @return void
-     * @see Показывает пример работы со String
-     * length() - длина строки
-     * toUpperCase() - в большие буквы
-     * toLowerCase() - в маленькие буквы  
-     * charAt() - символ по номеру
-     * substring() - часть строки
-     * contains() - проверяет есть ли слово
-     * equals() - сравнить
-     * replace() - заменить
-     * equalsIgnoreCase() - сравнить без учета больших/маленьких
-     * trim() - убрать пробелы по краям
+     * @see Показывает пример работы со StringBuilder
+     * append() - добавить в конец
+     * insert() - вставить в середину
+     * delete() - удалить часть
+     * reverse() - перевернуть строку
+     * replace() - заменить часть
+     * charAt() - получить символ по индексу
+     * length() - получить длину
+     * setCharAt() - изменить один символ
+     * substring() - получить часть строки
+     * capacity() - получить емкость
      */
     public static void main(String[] args) {
-        String text = "Hello WoRld!";
+        StringBuilder sb = new StringBuilder("Hello");
         
-        System.out.println("Длина: " + text.length());
+        sb.append(" World");
+        System.out.println("1. append(): " + sb);
         
-        System.out.println("Большие: " + text.toUpperCase());
-
-        System.out.println("Маленькие: " + text.toLowerCase());
-
-        System.out.println("Первый символ: " + text.charAt(0));
-
-        System.out.println("С 8 символа: " + text.substring(8));
-        System.out.println("С 0 по 6: " + text.substring(0, 6));
-
-        System.out.println("Есть 'WoRld!'? " + text.contains("WoRld!"));
-        System.out.println("Есть 'Python'? " + text.contains("Python"));
-
-        System.out.println("Заменить e на E: " + text.replace('e', 'E')); 
-
-        String text2 = "Hello world!";
-        System.out.println("Равны? " + text.equals(text2));
-
-        System.out.println("Равны без учета регистра? " + text.equalsIgnoreCase(text2));
-
-        String text3 = "   много пробелов   ";
-        System.out.println("Без пробелов: '" + text3.trim() + "'");
+        sb.insert(5, " Java");
+        System.out.println("2. insert(): " + sb);
+        
+        sb.delete(5, 10);
+        System.out.println("3. delete(): " + sb);
+        
+        sb.reverse();
+        System.out.println("4. reverse(): " + sb);
+        
+        sb.reverse();
+        System.out.println("5. reverse() обратно: " + sb);
+        
+        sb.replace(6, 11, "Java");
+        System.out.println("6. replace(): " + sb);
+        
+        System.out.println("7. charAt(0): " + sb.charAt(0));
+        
+        System.out.println("8. length(): " + sb.length());
+        
+        sb.setCharAt(0, 'h');
+        System.out.println("9. setCharAt(): " + sb);
+        
+        System.out.println("10. substring(0,5): " + sb.substring(0, 5));
+        
+        System.out.println("11. capacity(): " + sb.capacity());
+        
+        System.out.println("12. toString(): " + sb.toString());
     }
 }
